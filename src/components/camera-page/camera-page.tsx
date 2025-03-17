f97031f21d48e8ec4941d1fdf974fdcfb937f488
Nik NSM <nsm020598@gmail.com>
2025-03-17 13:00:21 +1000

    Добавит логику работы кнопки "наверх" для страницы товара

--- src/components/camera-page/camera-page.tsx
+++ src/components/camera-page/camera-page.tsx
@@ -66,7 +66,15 @@ export default function CameraPage(): JSX.Element {
             </>}
         </div>
       </main>
-      <a className="up-btn" href="#header">
+      <a className="up-btn" href="#header" onClick={(evt) => {
+        evt.preventDefault();
+        window.scrollTo({
+          top: 0,
+          left: 0,
+          behavior: 'smooth'
+        });
+      }}
+      >
         <svg width="12" height="18" aria-hidden="true">
           <use xlinkHref="#icon-arrow2"></use>
         </svg>
