83075e905f7b6f9ed3a8e59e524709c8ae20903b
Nik NSM <nsm020598@gmail.com>
2025-03-22 15:53:48 +1000

    Добавит тестирование "middleware redirect"

---
+++ src/store/middleware-redirect/redirect.test.ts
@@ -0,0 +1,45 @@
+import { MockStore, configureMockStore } from '@jedmao/redux-mock-store';
+import { TypeState } from '../../type/type-redux';
+import { redirect } from './redirect';
+import { AnyAction } from '@reduxjs/toolkit';
+import browserHistory from '../../browser-history/browser-history';
+import { actionRedirect } from './action-redirect';
+import { AddresesRoute } from '../../const';
+vi.mock('../../browser-history/browser-history', () => ({
+  default: {
+    location: { pathname: '' },
+    push(path: string) {
+      this.location.pathname = path;
+    }
+  }
+}));
+
+describe('Middleware redirect', () => {
+  let store: MockStore;
+
+  beforeAll(() => {
+    const middleware = [redirect];
+    const mockStoreCreator = configureMockStore<TypeState, AnyAction>(middleware);
+    store = mockStoreCreator();
+  });
+
+  beforeEach(() => {
+    browserHistory.push('');
+  });
+
+  it('should redirect to "/*" with action "actionRedirect"', () => {
+    const redirectToRoute = actionRedirect(AddresesRoute.PAGE_404);
+    store.dispatch(redirectToRoute);
+    expect(browserHistory.location.pathname).toBe(AddresesRoute.PAGE_404);
+  });
+
+  it('should not redirect to "/*" with empty action', () => {
+    const emptyAction = {
+      type: '',
+      payload: AddresesRoute.PAGE_404,
+    };
+    store.dispatch(emptyAction);
+    expect(browserHistory.location.pathname).not.toBe(AddresesRoute.PAGE_404);
+  });
+});
+
